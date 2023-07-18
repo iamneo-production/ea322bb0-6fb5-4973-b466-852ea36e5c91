ce02e0cc58bbee59f227935cee650e9a9af1a002
sai-dinesh472 <saidinesh3136@gmail.com>
2023-07-18 16:42:12 +0000

    commited

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-// import Jobs from './Pages/JobDetails/Components/Jobs';
-// import JobDetails from './Pages/JobDetails/Components/JobDetails';
-// import AdminPage from "./Pages/Dashboard/Admin/Admin";
-// import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
-// import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
-// import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
+import Jobs from './Pages/JobDetails/Components/Jobs';
+import JobDetails from './Pages/JobDetails/Components/JobDetails';
+import AdminPage from "./Pages/Dashboard/Admin/Admin";
+import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
+import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
+import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import JobSearchPage from './Pages/JobSearch/JobSearchPage';
 
@@ -14,18 +14,19 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Routes> */}
+        <Routes>
           {/* <Route exact path="/" element={<Jobs/>} />
           <Route exact path="/JobDetails/:id" element={<JobDetails/>} /> */}
 
-          {/* <Route path='/' element = {<AdminPage/>}/> */}
+          <Route path='/' element = {<AdminPage/>}/>
 
           {/* <Route path='/' element ={<HomePage/>}/>
           <Route path='/signup' element ={<SignUpPage/>}/>  
           <Route path='/login' element ={<Login/>}/>  */}
-          <JobSearchPage />
+          <Route path='/jobsearch' element = {<JobSearchPage />}/>
+          
 
-        {/* </Routes> */}
+        </Routes>
       </Router>   
     </div>
   );
