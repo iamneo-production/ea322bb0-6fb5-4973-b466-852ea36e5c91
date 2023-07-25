d0d7282973d4c12e01bd82f686e8d478a75513bf
sasivarunan390 <sasivarunan390@gmail.com>
2023-07-25 05:11:07 +0000

    Pushhed updated Jobseekers Dashboard

---
+++ reactapp/src/Pages/Dashboard/Jobseeker_Dashboard/Components/AppFooter/index.js
@@ -0,0 +1,11 @@
+
+import './index.css'
+import Footer from "../../../../LandingPage/Components/Footer";
+
+function AppFooter() {
+    return <div >
+       <Footer></Footer>
+    </div>;
+}
+
+export default AppFooter;
