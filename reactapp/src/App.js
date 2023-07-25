9e29aa401880fcfa46f25a6df3dd2a6f57d2322d
sasivarunan390 <sasivarunan390@gmail.com>
2023-07-25 04:57:20 +0000

    deleting jobseeker dash

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-// import Jobs from './Pages/JobDetails/Components/Jobs';
+ import './App.css';
+ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+ // import Jobs from './Pages/JobDetails/Components/Jobs';
 // import JobDetails from './Pages/JobDetails/Components/JobDetails';
-// import AdminPage from "./Pages/Dashboard/Admin/Admin";
+import Admin from "./Pages/Dashboard/Admin/Admin";
 // import HomePage from "./Pages/Signuplogindetails/Components/HomePage/HomePage";
 // import Login from "./Pages/Signuplogindetails/Components/LoginPage/Login";
 // import SignUpPage from "./Pages/Signuplogindetails/Components/SignUpPage/Signup";
@@ -15,36 +15,35 @@ import Signup from "./Pages/LandingPage/Components/Signup";
 import Contact from "./Pages/LandingPage/Components/Contact";
 import Login1 from "./Pages/LandingPage/Components/Login";
 import Errorpage from './Pages/LandingPage/Components/Errorpage';
-import JobseekerDashboard from "./Pages/Dashboard/Jobseeker_Dashboard/JobseekerDashboard";
+import JobseekerDashboard from './Pages/Dashboard/Jobseeker_Dashboard/JobseekerDashboard';
 //adding comment
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Routes>  
+      <Router> 
+         <Routes> 
           {/* <Route exact path="/" element={<Jobs/>} />
           <Route exact path="/JobDetails/:id" element={<JobDetails/>} /> */}
 
-          {/* <Route path='/' element = {<AdminPage/>}/> */}
+         {/*  <Route path='/' element = {<Admin/>}/> */}
 
           {/* <Route path='/' element ={<HomePage/>}/>
           <Route path='/signup' element ={<SignUpPage/>}/>  
           <Route path='/login' element ={<Login/>}/>  */}
           {/* <JobSearchPage /> */}
-        <Route path="/" element={<Home1 />} />
+       {/*  <Route path="/" element={<Home1 />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login1 />} />
-        <Route path="/*" element={<Errorpage/>}/>
-        
-          
-        {/* <JobseekerDashboard /> */}
+        <Route path="/*" element={<Errorpage/>}/> */}
+
+        <Route path="/" element={ <JobseekerDashboard/>}/>
        
-           </Routes>   
-      </Router>   
-    </div>
+          </Routes>  
+      </Router>  
+          </div>
   );
 }
 
--- reactapp/src/Pages/Dashboard/Jobseeker_Dashboard/Components/PagesContent/index.js
+++
@@ -1,9 +0,0 @@
-import AppRoutes from "../AppRoutes";
-
-function PageContent() {
-    return <div style={{marginTop:"3%"}} className="PageContent">
-        <AppRoutes />
-    </div>;
-}
-
-export default PageContent;
