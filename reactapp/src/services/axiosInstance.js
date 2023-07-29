c5797da29f70d9736aabb418071af82f71bdba25
sai-dinesh472 <saidinesh3136@gmail.com>
2023-07-29 14:18:46 +0000

    updated controllers

--- reactapp/src/services/axiosInstance.js
+++ reactapp/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://8080-fdaffbeddcdfefecdaaadaccdadddfabcfbf.project.examly.io",
+  baseURL: "https://8080-bfabbcefecdaaadaccdadddfabcfbf.project.examly.io",
   timeout: 5000,
   headers: {
     "Access-Control-Allow-Origin": "*",
