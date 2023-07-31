d05437992bbae44cc04d757497d38dbf3cb0401d
Arivalagan-G <arivalagan269@gmail.com>
2023-07-31 07:25:31 +0000

    Langing page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -10,7 +10,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import JobSearchPage from './Pages/JobSearch/JobSearchPage';
 import EmployerEditProfile from './Pages/Employer/Components/EmployerEditProfile';
 import EmployerProfile from './Pages/Employer/Components/EmployerProfile';
-
+import Home1 from "./Pages/LandingPage/Components/Home";
+import About from "./Pages/LandingPage/Components/About";
+ import Service from "./Pages/LandingPage/Components/Service";
+import Signup from "./Pages/LandingPage/Components/Signup";
+import Contact from "./Pages/LandingPage/Components/Contact";
+import Login1 from "./Pages/LandingPage/Components/Login";
+import Errorpage from './Pages/LandingPage/Components/Errorpage';
 
 function App() {
   return (
@@ -25,12 +31,20 @@ function App() {
           {/* <Route path='/' element ={<HomePage/>}/>
           <Route path='/signup' element ={<SignUpPage/>}/>  
           <Route path='/login' element ={<Login/>}/>  */}
-          <Routes>
+          {/* <Routes>
           <Route path="/empeditprofile" element={<EmployerEditProfile />} />
           <Route path="/empprofile/:id" element={<EmployerProfile />} />
-          </Routes>
-          {/*<JobSearchPage /> */}
-
+          </Routes> */}
+          {/* <JobSearchPage />  */}
+          <Routes>
+          <Route path="/" element={<Home1 />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login1 />} />
+        <Route path="/*" element={<Errorpage/>}/>
+        </Routes>
         {/* </Routes> */}
       </Router>   
     </div>
